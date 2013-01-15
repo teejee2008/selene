51cd6ad64b48ce92e3ed776091452644291c2a99
Tony George <teejee2008@gmail.com>
2013-01-15 22:43:55 +0530

    Updated scripts

--- src/selene-scripts/Convert to MKV (xvid & mp3).sh
+++
@@ -1,3 +0,0 @@
-
-avconv -i "${inFile}" -f matroska -acodec libmp3lame -vcodec libxvid -vf crop=iw:ih:0:0 -q 4 -qscale 4 -sn -y "${outDir}/${title}.mkv"
-
--- src/selene-scripts/Convert to MP3 Audio (LAME Encoder).sh
+++
@@ -1,2 +0,0 @@
-
-${audiodec} | lame --nohist --brief -V 4 -q 5 --replaygain-fast - "${outDir}/${title}.mp3"
