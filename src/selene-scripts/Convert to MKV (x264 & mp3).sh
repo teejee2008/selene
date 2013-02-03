3d4b054626e92f6717402a2862f2f660b4dae75a
Tony George <teejee2008@gmail.com>
2013-02-03 19:05:41 +0530

    Update scripts to use LAME encoder instead of lavc, audio will be converted to stereo

--- src/selene-scripts/Convert to MKV (x264 & mp3).sh
+++ src/selene-scripts/Convert to MKV (x264 & mp3).sh
@@ -3,7 +3,7 @@ tempAudio="${tempDir}/${title}.mp3"
 tempVideo="${tempDir}/${title}.mkv"
 
 if [ "${hasAudio}" == "1" ]; then
-	${audiodec} | lame --nohist --brief -V 4 -q 5 --replaygain-fast - "${tempAudio}"
+	avconv -i "${inFile}" -f wav -acodec pcm_s16le -ac 2 -vn -y - | lame --nohist --brief -V 4 -q 5 --replaygain-fast - "${tempAudio}"
 fi
 
 x264 --vf crop:0,0,0,0 -o "${tempVideo}" "${inFile}"
--- src/selene-scripts/Convert to MKV (x264 & mp3, 700 MB).sh
+++ src/selene-scripts/Convert to MKV (x264 & mp3, 700 MB).sh
@@ -3,7 +3,7 @@ tempAudio="${tempDir}/${title}.mp3"
 tempVideo="${tempDir}/${title}.mkv"
 targetMB=700
 
-${audiodec} | lame --nohist --brief -V 4 -q 5 --replaygain-fast - "${tempAudio}"
+avconv -i "${inFile}" -f wav -acodec pcm_s16le -ac 2 -vn -y - | lame --nohist --brief -V 4 -q 5 --replaygain-fast - "${tempAudio}"
 
 echo "==========================================================">&2
 audioSize=$(stat -c%s "${tempAudio}")
