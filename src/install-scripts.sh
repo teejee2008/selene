79c35b58c52cd5b140584df3d2e70fcd8eb4b260
Tony George <teejee2008@gmail.com>
2013-03-30 10:05:22 +0530

    Update scripts

--- src/install-scripts.sh
+++ src/install-scripts.sh
@@ -1,3 +1,6 @@
+
+sudo rm -r /usr/share/selene/scripts/*
+sudo rm -r /usr/share/selene/presets/*
 sudo cp -dpr --no-preserve=ownership -t /usr/share/selene/scripts scripts/*
 sudo cp -dpr --no-preserve=ownership -t /usr/share/selene/presets presets/*
 sudo chmod --recursive 0755 /usr/share/selene/*
