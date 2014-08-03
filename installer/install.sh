16417a951675a8b1d191bfd06c41e1c4bb44bd31
Tony George <tony.george.kol@gmail.com>
2014-08-03 14:32:05 +0530

    Updated install script; Added uninstaller

--- installer/install.sh
+++ installer/install.sh
@@ -1,26 +1,163 @@
 #!/bin/bash
 
-backup=`pwd`
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd "$DIR"
+app_name='selene'
+app_fullname='Selene'
 
-echo "Installing files..."
+generic_depends=(mediainfo libav-tools vorbis-tools x264 lame mkvtoolnix realpath rsync libgee json-glib)
+debian_depends=(realpath mediainfo libav-tools vorbis-tools x264 lame mkvtoolnix rsync libgee2 libjson-glib-1.0-0)
+redhat_depends=(rsync libgee json-glib mediainfo vorbis-tools mkvtoolnix)
+arch_depends=(rsync libgee json-glib mediainfo x264 ffmpeg vorbis-tools lame mkvtoolnix boost icu extra/libstdc++5)
 
-sudo cp -dpr --no-preserve=ownership -t / ./*
+generic_recommends=(opus-tools vpx-tools ffmpeg2theora gpac sox)
+debian_recommends=(opus-tools vpx-tools ffmpeg2theora gpac sox)
+redhat_recommends=(opus-tools libvpx-utils theora-tools sox)
+arch_recommends=(ffmpeg2theora community/opus-tools extra/libvpx gpac sox)
+
+Reset='\e[0m'
+Red='\e[1;31m'
+Green='\e[1;32m'
+Yellow='\e[1;33m'
+
+CHECK_COLOR_SUPPORT() {
+	colors=`tput colors`
+	if [ $colors -gt 1 ]; then
+		COLORS_SUPPORTED=0
+	else
+		COLORS_SUPPORTED=1
+	fi
+}
+
+MSG_INFO() {
+	if [ $COLORS_SUPPORTED -eq 0 ]; then
+        echo -e "[${Yellow}*${Reset}] ${Green}$1${Reset}"
+    else
+        echo -e "[*] $1"
+    fi
+}
+
+MSG_WARNING() {
+	if [ $COLORS_SUPPORTED -eq 0 ]; then
+        echo -e "[${Red}!${Reset}] ${Yellow}$1${Reset}"
+    else
+        echo -e "[!] $1"
+    fi
+}
+
+MSG_ERROR() {
+    if [ $COLORS_SUPPORTED -eq 0 ]; then
+        echo -e "[${Red}X${Reset}] ${Yellow}$1${Reset}"
+    else
+        echo -e "[X] $1"
+    fi
+}
+
+CD_PUSH() {
+	cd_backup=`pwd`
+}
+
+CD_POP() {
+	if [ ! -z "${cd_backup}" ]; then
+		cd "${cd_backup}"
+	fi
+}
+
+EXIT(){
+	CD_POP
+	exit $1
+}
+
+WAIT_FOR_INPUT() {
+	echo ""
+	echo "Press any key to exit..."
+	read dummy
+}
+
+GET_SCRIPT_PATH(){
+	SCRIPTPATH="$(cd "$(dirname "$0")" && pwd)"
+	SCRIPTNAME=`basename $0`
+}
+
+RUN_AS_ADMIN() {
+	if [ ! `id -u` -eq 0 ]; then
+		GET_SCRIPT_PATH
+		if command -v sudo >/dev/null 2>&1; then
+			sudo "${SCRIPTPATH}/${SCRIPTNAME}"
+			EXIT $?
+		elif command -v su >/dev/null 2>&1; then
+			su -c "${SCRIPTPATH}/${SCRIPTNAME}"
+			EXIT $?
+		else
+			echo ""
+			MSG_ERROR "** Installer must be run as Admin (using 'sudo' or 'su') **"
+			echo ""
+			EXIT 1
+		fi
+	fi
+}
+
+CD_PUSH
+CHECK_COLOR_SUPPORT
+RUN_AS_ADMIN
+
+MSG_INFO "Expanding directories..."
+for f in `find ./ -type d -exec echo {} \;`; do
+    directory=`echo "$f" | sed -r 's/^.{2}//'`
+    mkdir -p -m 755 "/$directory"
+    echo "/$directory"
+done
+echo ""
+
+MSG_INFO "Installing files..."
+for f in `find ./ -type f \( ! -iname "install.sh" \) -exec echo {} \;`; do
+    file=`echo "$f" | sed -r 's/^.{2}//'`
+    install -m 0755 "./$file" "/$file"
+    echo "/$file"
+done
+echo ""
+
+if [ -f /etc/debian_version ]; then
+	if command -v apt-get >/dev/null 2>&1; then
+		MSG_INFO "Installing Debian packages..."
+		echo ""
+		for i in "${debian_depends[@]}"; do
+		  MSG_INFO "Installing: $i"
+		  apt-get -y install $i
+		done
+	fi
+elif [ -f /etc/redhat-release ]; then
+	if command -v yum >/dev/null 2>&1; then
+		MSG_INFO "Installing RedHat packages..."
+		echo ""
+		for i in "${redhat_depends[@]}"; do
+		  MSG_INFO "Installing: $i"
+		  yum -y install $i
+		done
+	fi
+elif [ -f /etc/arch-release ] || [ -f /etc/manjaro-release ]; then
+	if command -v pacman >/dev/null 2>&1; then
+		MSG_INFO "Installing ArchLinux packages..."
+		echo ""
+		for i in "${arch_depends[@]}"; do
+		  MSG_INFO "Installing: $i"
+		  pacman -S --noconfirm $i
+		done
+	fi
+fi
+echo ""
 
 if [ $? -eq 0 ]; then
-	echo "Installed successfully."
+	MSG_INFO "Installed successfully."
 	echo ""
-	echo "Start Selene using the shortcut in the application menu"
-	echo "or by typing 'selene' in a terminal window"	
-	echo ""
-	echo "Following packages are required for this application to function correctly:"
-	echo "- (Required) libgtk-3, libgee2, libsoup, libjson-glib, realpath, rsync, mediainfo, libav-tools"
-	echo "- (Optional) vorbis-tools, opus-tools, vpx-tools, x264, lame, mkvtoolnix, ffmpeg2theora, gpac, rsync"
-	echo "Please ensure that the required packages are installed and up-to-date"
+	MSG_INFO "Start ${app_fullname} using the shortcut in the Applications Menu"
+	MSG_INFO "or by running the command: ${app_name}"	
+	MSG_INFO "If it fails to start, check and install following packages:"
+	MSG_WARN "Required: ${generic_depends[@]}"
+	MSG_WARN "Optional: ${generic_recommends[@]}"
+	WAIT_FOR_INPUT
+	EXIT 0
 else
-	echo "Installation failed!"
-	exit 1
+	MSG_ERROR "Install completed with errors"
+	EXIT 1
 fi
 
-cd "$backup"
+CD_POP
